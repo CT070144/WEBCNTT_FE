4ee176a9045ad08a205e80b764d45bdf00325f7d
An <apbd2003123@gmail.com>
2025-03-04 15:57:49 +0700

    build lai lan thu n

--- src/pages/Posts/components/PostDetail/index.js
+++ src/pages/Posts/components/PostDetail/index.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import classNames from "classnames/bind";
 import styles from "./PostDetail.module.scss";
 
-
 function PostDetail() {
     const cx = classNames.bind(styles);
     const { slug } = useParams();
